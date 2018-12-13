1bbd87ac43cb5b4c17f480203aa0ee28a5d6bd69
Guillermo Cirer <guille.mca@hotmail.com>
2018-12-13 22:04:56 +0100

    allow cors

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,15 @@ from config import ProductionConfig
 app = Flask(__name__)
 app.config.from_object(ProductionConfig)
 
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
+
 db = MongoEngine(app)
 jwt = JWTManager(app)
 
